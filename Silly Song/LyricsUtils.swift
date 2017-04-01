386327df0d4ad238aa00f02c8914bf2660fba2b5
JobsJr <r.rajeev434@gmail.com>
2017-04-01 11:48:39 +0530

    First Commit

---
+++ Silly Song/LyricsUtils.swift
@@ -0,0 +1,47 @@
+//
+//  LyricsUtils.swift
+//  Silly Song
+//
+//  Created by Rajeev Kr on 29/03/17.
+//  Copyright © 2017 Rajeev Kr. All rights reserved.
+//
+
+import Foundation
+
+class LyricsUtils{
+    private static let lyricsTemplate =  ["<FULL_NAME>, <FULL_NAME>, Bo B<SHORT_NAME>",
+                                                "Banana Fana Fo F<SHORT_NAME>",
+                                                "Me My Mo M<SHORT_NAME>",
+                                                "<FULL_NAME>"].joined(separator: "\n")
+    
+     static func  shortNameFromName(name: String) -> String {
+        let newName = name.lowercased().folding(options: .diacriticInsensitive, locale: .current)
+        
+        let vowels = "aeiou"
+        let nameInCharset = CharacterSet(charactersIn: vowels)
+        if let startIndex = newName.lowercased().rangeOfCharacter(from: nameInCharset)?.lowerBound{
+            
+            return  name.substring(from:startIndex).lowercased()
+        }
+        return ""
+    }
+    
+    static func lyricsForName(fullName: String) -> String {
+        let shortName = shortNameFromName(name:fullName)
+        let fullNameFormat = "<FULL_NAME>"
+        let shortNameFormat = "<SHORT_NAME>"
+        
+        if(shortName != ""){
+            
+            let lyrics = lyricsTemplate
+                .replacingOccurrences(of: fullNameFormat, with: fullName, options: .caseInsensitive, range: nil)
+                .replacingOccurrences(of: shortNameFormat, with: shortName, options: .caseInsensitive, range: nil)
+            return lyrics
+        }
+        
+        
+        
+        return ""
+    }
+
+}
--- Silly Song/ViewController.swift
+++ Silly Song/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITextFieldDelegate {
+    @IBOutlet weak var mNameField: UITextField!
+    @IBOutlet weak var mLyricsView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mNameField.delegate = self
+        mNameField.autocorrectionType = .no
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func handleReset(_ sender: UITextField) {
+        mNameField.text = ""
+        mLyricsView.text = ""
+        
+    }
 
+    @IBAction func handleLyricsDisplay(_ sender: UITextField) {
+        let name = mNameField.text
+        if (name != nil) && name != ""{
+            let sillyLyrics = LyricsUtils.lyricsForName(fullName: name!)
+            if(sillyLyrics != ""){
+                mLyricsView.text = sillyLyrics
+            }
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
 }
 
